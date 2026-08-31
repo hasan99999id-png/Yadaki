6e01d56dfaca05334d067befb3685c7de68fe880
Moochi_bazar <hasan99999id@gmail.com>
2026-09-01 00:13:30 +0330

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+/* بعد از Deploy کردن Google Apps Script، آدرس /exec را اینجا قرار دهید. */
+window.APP_CONFIG = {
+  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  STORE_NAME: 'V_PLUS'
+};
